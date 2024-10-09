a343f6a80d6b7824e7d2d68aff32be85b101902c
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-10-09 14:03:22 +0200

    Preview

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,11 @@
 module Main exposing (main)
 
 import Browser
-import Element exposing (Element, alignTop, column, fill, height, padding, paragraph, px, row, spacing, text, textColumn, width, wrappedRow)
+import Element exposing (Element, alignBottom, alignTop, column, el, fill, fillPortion, height, padding, paragraph, px, row, spacing, text, textColumn, width, wrappedRow)
+import Element.Font as Font
 import Element.Input as Input
+import Html
+import Html.Parser
 import List.Extra
 
 
@@ -37,17 +40,46 @@ init =
 
 view : Model -> Element Msg
 view model =
-    row
+    let
+        stringVersion : String
+        stringVersion =
+            modelToString model
+    in
+    column
         [ spacing 8
         , width fill
         , height fill
         ]
-        [ viewLines model
-        , modelToString model
-            |> String.split "\n"
-            |> List.map (\line -> paragraph [] [ text line ])
-            |> textColumn [ width fill, alignTop ]
+        [ row
+            [ spacing 8
+            , width fill
+            , height fill
+            ]
+            [ viewLines model
+            , stringVersion
+                |> String.split "\n"
+                |> List.map (\line -> paragraph [] [ text line ])
+                |> textColumn
+                    [ width fill
+                    , alignTop
+                    , Font.size 14
+                    ]
+            ]
+        , viewPreview stringVersion
         ]
+
+
+viewPreview : String -> Element Msg
+viewPreview input =
+    case Html.Parser.run Html.Parser.allCharRefs input of
+        Ok parsed ->
+            Html.Parser.nodesToHtml parsed
+                |> Html.div []
+                |> Element.html
+                |> el [ width fill, alignBottom ]
+
+        Err _ ->
+            text "Could not parse HTML"
 
 
 viewLines : Model -> Element Msg
@@ -67,7 +99,7 @@ viewLines model =
             )
         |> column
             [ spacing 8
-            , width fill
+            , width (fillPortion 3)
             , alignTop
             ]
 
@@ -88,7 +120,7 @@ viewLine chunks =
                         )
             )
         |> wrappedRow
-            [ spacing 8
+            [ spacing 2
             , width fill
             ]
 
@@ -102,9 +134,13 @@ viewChunk ( text, ruby ) =
                 px 40
 
             else
-                fill
+                Element.minimum
+                    (max 50
+                        (10 * max (String.length text) (String.length ruby))
+                    )
+                    fill
     in
-    column [ spacing 4, width w ]
+    column [ spacing 2, width w ]
         [ Input.text [ width w ]
             { label = Input.labelHidden ""
             , text = ruby
@@ -134,7 +170,7 @@ lineToString chunks =
         middle =
             chunks
                 |> List.map chunkToString
-                |> String.join " "
+                |> String.concat
     in
     "<p>" ++ middle ++ "</p>"
 
